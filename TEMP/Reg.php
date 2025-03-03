74a98f0ee68e4fd1169a302ea057011ac559eb64
Matteo Cardozo <matteo.gomez@fing.edu.uy>
2025-03-03 00:04:21 +0000

    reg template

--- TEMP/Reg.php
+++ TEMP/Reg.php
@@ -1,16 +1,19 @@
 <div class="container d-flex justify-content-center flex-col p-1 p-sm-5">
     <div class="row"> 
-        <div class="col-12 col-sm-6 text-center text-light d-flex justify-content-center align-items-center">
-            <h1 class="text-light">La tipica imagen abstracta:</h1>
-            <img src="../PUBLIC/" class="img-fluid rounded" alt="...">
+        <div class="p-3 col-12 col-sm-6 text-center text-light flex flex-col justify-content-center align-items-center">
+            <h1 class="text-light pb-4">tipica imagen abstracta</h1>
+            <img 
+                src="http://img.freepik.com/free-vector/hand-drawn-matisse-style-illustration_23-2149569710.jpg?t=st=1740959952~exp=1740963552~hmac=b9cfe97c31eaab7251f1e6d3a9dab5c90c66ec44674f77d50278dd5cce8a5841&w=740" 
+                class="img-fluid" style="border-radius: 10%;" alt="tipica imagen abstracta"
+            >
         </div>
         <div class="col-12 col-sm-6">
-            <div class="card text-dark rounded-xl p-3" style="background-color:rgb(222, 231, 240);">
+            <div class="card text-dark p-3 bg-gradient" style="border-radius: 5%;">
                 <div class="card-body p-4 text-dark  justify-content-center">
                     <h2 class="card-title text-dark text-center">Ingrese los datos de registro</h2>
-                    <form>
+                    <form action="registro-usuario" method="POST">
                         <div class="form-group">
-                            <label for="username" class="form-label text-dark">Nombre de Usuario</label>
+                            <label for="username" class="form-label">Nombre de Usuario</label>
                             <input type="text" class="form-control" id="username" placeholder="Ejemplo: Juanito123">
                         </div>
                         <div class="form-group">
