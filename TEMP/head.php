1436fe1e978d913f0bc2faffc14361ef17c34814
Matteo Cardozo <matteo.gomez@fing.edu.uy>
2025-03-23 15:59:21 +0000

    Mock para mostrar heroes de marvel Commit con changes a marvel-api.js y estilos

--- TEMP/head.php
+++ TEMP/head.php
@@ -1,4 +1,4 @@
-<head style="background-color:rgb(39, 37, 46);" class="text-dark">
+<head class="text-light">
     <title>La proxima peli de Marvel: <?=$title;?></title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,6 +9,7 @@
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
         crossorigin="anonymous"
     />   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
